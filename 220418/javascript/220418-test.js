d4ac71b207be4df852fb1b4785e803c5569be20f
K1mjueun <jueun4779@gmail.com>
2022-04-18 16:02:40 +0900

    javascript에 다가가기..

---
+++ 220418/javascript/220418-test.js
@@ -0,0 +1,29 @@
+// let classtime = 8;
+// let classdate = 20;
+// let classmonth = 6;
+
+// let age = 25;
+// let born = 1998;
+// let BD = 1210;
+
+// let onion = 1300;
+// let cheese = 4300;
+// let water = 8230;
+// let tissue = 5450;
+// let apple = 3200;
+// let won = '원';
+
+// console.log((onion+cheese+water+tissue+apple)/2+won);
+//계산기 연습
+
+const jueun = {
+  name:"김주은",
+  like:"animal",
+  hate:"insect"
+}
+
+console.log(jueun.name +" 안녀엉");
+// console.log(jueun.like +" is so lovely");
+// console.log(jueun.hate +" is so terrible");
+
+
