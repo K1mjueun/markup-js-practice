e35aaf726f47ceba90a5117cfbf4a1acdcfad477
K1mjueun <jueun4779@gmail.com>
2022-05-24 10:42:22 +0900

    이런것이 객체지향형 프로그래밍

--- directory-setting/js/module/b.js
+++ directory-setting/js/module/b.js
@@ -3,7 +3,11 @@ const mainObject = {
   fileName : "b.js",
   function : "export, import",
   explain : "자바스크립트는 내보내기 가져오기 기능을 지원합니다."
+  test : function() {
+    console.log('객체지향형 프로그래밍');
+  }
 }
 
 
 export default mainObject
+// ;을 쓰지 않아도 된다
