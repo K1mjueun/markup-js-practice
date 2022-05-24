97a7f77878dd0b8dc90c8e35405fb0e13926d089
K1mjueun <jueun4779@gmail.com>
2022-05-24 10:46:02 +0900

    큰 파일 관리할때 사용하는 방식

--- directory-setting/js/script.js
+++ directory-setting/js/script.js
@@ -1,7 +1,10 @@
 import { aData } from "./module/a.js";
 console.log(aData);
+
 import bData from "./module/b.js";
 console.log(bData);
+// * ↑ 큰 파일을 관리할 때 매우 많이 사용하는 방식
+
 import {
   cFunc,
   testVariable,
