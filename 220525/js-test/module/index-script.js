90ecf8549c4db43c54dd7e2e0bc8bab919007fbb
K1mjueun <jueun4779@gmail.com>
2022-05-25 11:44:38 +0900

    js 파일 관리 연습 / export와 import

---
+++ 220525/js-test/module/index-script.js
@@ -0,0 +1,9 @@
+import {work} from "./test.js";
+
+window.addEventListener('load', function() {
+
+});
+//윈도우가 로드됬을때 이벤트
+
+console.log('test');
+console.log(work);
---
+++ 220525/js-test/module/test.js
@@ -0,0 +1,8 @@
+export const work = 'work';
+
+// export const sumFunction = function(a,b) {
+//   return a+b;
+// }
+//코드를 일일히 쓰지 않고 코드 재활용성을 극대화 시킬 수 있다
+
+//나의 부품 보따리가 생성된다
