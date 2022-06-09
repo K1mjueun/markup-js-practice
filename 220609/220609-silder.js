c2e98784deefdcce74b2587c3cf8e2600fd89341
K1mjueun <jueun4779@gmail.com>
2022-06-09 12:19:59 +0900

    html 구조 수정

--- 220609/220609-silder.js
+++ 220609/220609-silder.js
@@ -1,20 +1,23 @@
 
 const root = document.getElementById('root');
 const leftBtn = root.children[0];
-const rightBtn = root.children[6];
+const rightBtn = root.children[2];
 
-const mockupImg1 = root.children[1];
-const mockupImg2 = root.children[2];
-const mockupImg3 = root.children[3];
-const mockupImg4 = root.children[4];
-const mockupImg5 = root.children[5];
+const slide = document.getElementById('slide');
+const mockupImg1 = slide.children[0];
+const mockupImg2 = slide.children[1];
+const mockupImg3 = slide.children[2];
+const mockupImg4 = slide.children[3];
+const mockupImg5 = slide.children[4];
 
 const imgArr = [mockupImg1, mockupImg2, mockupImg3, mockupImg4, mockupImg5];
+Array.from(imgArr);
 
 leftBtn.addEventListener("click", function() {
   let temp;
 
   temp = imgArr[0];
+  //temp에 imgArr 배열 맨 앞 요소 대입
   imgArr.shift();
   //shift - 배열 맨 앞의 요소 제거
   imgArr.push(temp);
@@ -27,6 +30,7 @@ rightBtn.addEventListener("click", function() {
   let temp;
 
   temp = imgArr[imgArr.length-1];
+  //temp에 imgArr 배열 맨 뒤 요소 대입
   imgArr.pop();
   //pop - 배열 맨 뒤의 요소 제거
   imgArr.unshift(temp);
