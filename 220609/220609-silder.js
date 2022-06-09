e91d6676c424fa0028b1e984066f1cfa4da101c0
K1mjueun <jueun4779@gmail.com>
2022-06-09 12:56:53 +0900

    문제 찾기, 이해하기

--- 220609/220609-silder.js
+++ 220609/220609-silder.js
@@ -12,35 +12,42 @@ const mockupImg5 = slide.children[4];
 
 const imgArr = [mockupImg1, mockupImg2, mockupImg3, mockupImg4, mockupImg5];
 Array.from(imgArr);
+console.log(imgArr);
 
-leftBtn.addEventListener("click", function() {
+rightBtn.addEventListener("click", function() {
   let temp;
-
   temp = imgArr[0];
-  //temp에 imgArr 배열 맨 앞 요소 대입
+
   imgArr.shift();
   //shift - 배열 맨 앞의 요소 제거
+  //?mockupImg1이 배열 맨 앞이니까 제거되겠지
   imgArr.push(temp);
   //push - 배열 맨 뒤에 요소 추가
+  //?temp를 추가하는 거니까 mockupImg1이 배열 맨 뒤로 추가되겠지
 
   imgArr[0].style.display = "flex";
+  //?배열 맨 앞의 요소는 mockupImg2가 되었으니 mockupImg2의 display가 flex가 되겠지
   imgArr[imgArr.length-1].style.display = "none";
+  //?배열 맨 뒤의 요소는 mockupImg1이 되었으니 mockupImg1의 display가 none이 되겠지
   
   console.log(imgArr);
 });
 
-rightBtn.addEventListener("click", function() {
+leftBtn.addEventListener("click", function() {
   let temp;
-
   temp = imgArr[imgArr.length-1];
-  //temp에 imgArr 배열 맨 뒤 요소 대입
+
   imgArr.pop();
   //pop - 배열 맨 뒤의 요소 제거
+  //?mockupImg5가 배열 맨 뒤니까 제거되겠지
   imgArr.unshift(temp);
   //unshift - 배열 맨 앞에 요소 추가
+  //?temp를 추가하는 거니까 mockupImg4가 배열 맨 앞으로 추가되겠지
 
   imgArr[0].style.display = "none";
+  //?배열 맨 앞의 요소는 mockupImg4가 되었으니 mockupImg4의 display가 none이 되겠지
   imgArr[imgArr.length-1].style.display = "flex";
+  //?배열 맨 뒤의 요소는 mockupImg3이 되었으니 mockupImg3의 display가 flex가 되겠지
 
   console.log(imgArr);
 })
