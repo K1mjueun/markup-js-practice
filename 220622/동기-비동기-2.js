8b7fc29975cbd09617490b83d81b22ba7ad5c139
K1mjueun <jueun4779@gmail.com>
2022-06-22 11:29:12 +0900

    순서를 지킬 땐 Promise!!

--- 220622/동기-비동기-2.js
+++ 220622/동기-비동기-2.js
@@ -5,3 +5,23 @@ let mandoo = 100;
 // work .2 "1초 뒤에" 2번 행동에는 mandoo를 30을 빼고 콘솔이 출력된다 / 80
 // todo : 총 2초간 두번의 계산을 진행시킨다
 
+function first() {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      mandoo = mandoo + 10;
+      console.log(mandoo);
+      resolve();
+    },1000);
+  });
+}
+
+first()
+  .then(function(data) {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        mandoo = mandoo - 30;
+        console.log(mandoo);
+        resolve();
+      },1000);
+    });
+  }) 
