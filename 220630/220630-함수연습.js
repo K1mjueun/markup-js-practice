ec0a63b725926c9513bfbc164857f3d3804ee779
K1mjueun <jueun4779@gmail.com>
2022-06-30 15:00:29 +0900

    함수연습

--- 220630/220630-함수연습.js
+++ 220630/220630-함수연습.js
@@ -8,13 +8,20 @@
 
 // console.log(one() === "메롱");
 
-function realone(ipgoo) {
+// function realone(ipgoo) {
   
-  if(ipgoo === "바보") {
-    return "바보아니야";
-  } else {
-    return "천재";
-  }
+//   if(ipgoo === "바보") {
+//     return "바보아니야";
+//   } else {
+//     return "천재";
+//   }
+// }
+
+// console.log(realone("바보"));
+
+function one(ipgoo, ipgootoo) {
+
+  return ipgoo+ipgootoo
 }
 
-console.log(realone("바보"));
+console.log(one("아버지가 ","방에 들어가신다"));
