99cfc0c50b54ec07d1649f56c8d88537e72e1d8d
K1mjueun <jueun4779@gmail.com>
2022-06-30 14:55:40 +0900

    함수 이해

---
+++ 220630/220630-함수연습.js
@@ -0,0 +1,20 @@
+
+// function one() {
+
+//   console.log("야호");
+
+//   return "메롱";
+// }
+
+// console.log(one() === "메롱");
+
+function realone(ipgoo) {
+  
+  if(ipgoo === "바보") {
+    return "바보아니야";
+  } else {
+    return "천재";
+  }
+}
+
+console.log(realone("바보"));
