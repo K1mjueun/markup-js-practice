26966ce8e1d4efb6269a724c5f7abfa3420b4c0f
K1mjueun <jueun4779@gmail.com>
2022-07-07 10:10:55 +0900

    reverse나 변수를 사용해 배열 순서 뒤집기

---
+++ 220707/reverse-temp.js
@@ -0,0 +1,14 @@
+
+let mandoo = [1,2];
+
+console.log(mandoo.reverse());
+// reverse는 배열의 순서를 뒤집는다
+
+
+let temp;
+let ahjin = [1,2];
+temp = ahjin[0];
+ahjin[0] = ahjin[1];
+ahjin[1] = temp;
+console.log(ahjin);
+//변수 만들어서 바꾸기
